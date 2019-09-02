63a3b934029cc3396352bf95dce5553509f1a6ed
KiritchoukC <clement.kiritchouk@gmail.com>
2019-09-02 17:10:57 +0200

    Erase bootstrap files + update packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,7 +59,7 @@ module.exports = {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -69,7 +69,8 @@ module.exports = {
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
-        }
+        },
+        light: {}
       }
     }
   },
