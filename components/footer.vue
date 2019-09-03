ac88e45cf492e047d9f32b29a72cdfda9caa9258
KiritchoukC <clement.kiritchouk@gmail.com>
2019-09-03 23:32:02 +0200

    edit footer style

--- components/footer.vue
+++ components/footer.vue
@@ -1,32 +1,64 @@
 <template>
-  <v-footer app>
-    <span>Kiritchouk Clément - &copy; 2019</span>
-    <div>
-      Github Icon made by
-      <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy"
-        >Dave Gandy</a
-      >
-      from
-      <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-    </div>
-    <div>
-      LinkedIn Icon made by
-      <a href="https://www.flaticon.com/authors/simpleicon" title="SimpleIcon"
-        >SimpleIcon</a
-      >
-      from
-      <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-    </div>
-    <div>
-      Twitter Icon made by
-      <a
-        href="https://www.flaticon.com/authors/pixel-perfect"
-        title="Pixel perfect"
-        >Pixel perfect</a
-      >
-      from
-      <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
-    </div>
+  <v-footer dark absolute app color="rgb(7, 43, 32)">
+    <v-row justify="center" align="center">
+      <v-col offset="4" cols="4" align-self="center">
+        <v-row justify="center" align="center">
+          <p>Kiritchouk Clément - &copy; 2019</p>
+        </v-row>
+      </v-col>
+      <v-col cols="4">
+        <v-row>
+          <v-col>
+            Github Icon made by
+            <a
+              href="https://www.flaticon.com/authors/dave-gandy"
+              title="Dave Gandy"
+              class="grey--text"
+              >Dave Gandy</a
+            >
+            from
+            <a
+              href="https://www.flaticon.com/"
+              title="Flaticon"
+              class="grey--text"
+              >www.flaticon.com</a
+            >
+          </v-col>
+          <v-col>
+            Twitter Icon made by
+            <a
+              href="https://www.flaticon.com/authors/pixel-perfect"
+              title="Pixel perfect"
+              class="grey--text"
+              >Pixel perfect</a
+            >
+            from
+            <a
+              href="https://www.flaticon.com/"
+              title="Flaticon"
+              class="grey--text"
+              >www.flaticon.com</a
+            >
+          </v-col>
+          <v-col>
+            LinkedIn Icon made by
+            <a
+              href="https://www.flaticon.com/authors/simpleicon"
+              title="SimpleIcon"
+              class="grey--text"
+              >SimpleIcon</a
+            >
+            from
+            <a
+              href="https://www.flaticon.com/"
+              title="Flaticon"
+              class="grey--text"
+              >www.flaticon.com</a
+            >
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-footer>
 </template>
 
